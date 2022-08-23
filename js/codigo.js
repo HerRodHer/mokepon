201ae15f0056b4db914c615156bba14b3d15d97f
El Perro <hernaez.rod@gmail.com>
2022-08-23 23:38:33 +0200

    Commit 06 - .JS: Seleccionador de mascota aleatoria

--- js/codigo.js
+++ js/codigo.js
@@ -1,3 +1,4 @@
+const mascotas = [ "Hipoperro", "Capipepo", "Ratigueya", "Langosucio", "Tucanazo", 'Pedos' ]
 
 function iniciarJuego()
 {
@@ -18,7 +19,6 @@ function seleccionarMascotaJugador()
     let spanMascotaJugador = document.getElementById('mascota-jugador')
 
     const inputs = [ inputHipoperro, inputCapipepo, inputRatigueya, inputLangosucio, inputTucanazo, inputPedos ]
-    const mascotas = [ "Hipoperro", "Capipepo", "Ratigueya", "Langosucio", "Tucanazo", 'Pedos' ]
 
     for (i = 0; i < mascotas.length; i++)
     {
@@ -26,32 +26,27 @@ function seleccionarMascotaJugador()
         {
             mascotaSeleccionada = mascotas[i]
             spanMascotaJugador.innerHTML = mascotaSeleccionada
+            seleccionarMascotaEnemigo()
             break
         }
     }
-    alert("Elegiste a: " + mascotaSeleccionada)
-
     if (mascotaSeleccionada == "Ninguna")
     {
         alert("Selecciona una mascota, por favor")
-    }
+        return 1;
+    }  
+}
+
+function seleccionarMascotaEnemigo()
+{
+    let mascotaAleatoria = aleatorio(1,6)
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+    spanMascotaEnemigo.innerHTML = mascotas[mascotaAleatoria - 1]
+}
+
+function aleatorio(min, max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min)
 }
 
 window.addEventListener('load', iniciarJuego)
-
-
-
-/*
-    if (inputHipoperro.checked)
-        alert('Elegiste a Hipoperro')
-    if (inputCapipepo.checked)
-        alert('Elegiste a Capipepo')
-    if (inputRatigueya.checked)
-        alert('Elegiste a Ratigueya')
-    if (inputLangosucio.checked)
-        alert('Elegiste a Langosucio')
-    if (inputTucanazo.checked)
-        alert('Elegiste a Tucanazo')
-    if (inputPedos.checked)
-        alert('Elegiste a Pedos')
-*/
