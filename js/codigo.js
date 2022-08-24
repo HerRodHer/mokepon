26003ea39e73442b8e20cd66e1778dff9018ddbd
El Perro <hernaez.rod@gmail.com>
2022-08-24 21:00:37 +0200

    Commit 08 - Combat / Result / Translation

--- js/codigo.js
+++ js/codigo.js
@@ -1,14 +1,14 @@
-const mascotas = [ "Hipoperro", "Capipepo", "Ratigueya", "Langosucio", "Tucanazo", 'Pedos' ]
-let ataqueJugador
-let ataqueEnemigo
+const pets = [ "Hipoperro", "Capipepo", "Ratigueya", "Langosucio", "Tucanazo", 'Pedos' ]
+let attackPlayer
+let attackEnemy
+let attackResult
 
-function iniciarJuego()
+function startGame()
 {
-    let botonMascotaJugador = document.getElementById('boton-mascota')
-    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+    let buttonPetPlayer = document.getElementById('boton-mascota')
+    buttonPetPlayer.addEventListener('click', selectPetPlayer)
 }
-
-function seleccionarMascotaJugador()
+function selectPetPlayer()
 {
     let inputHipoperro = document.getElementById('hipoperro')
     let inputCapipepo = document.getElementById('capipepo')
@@ -17,77 +17,88 @@ function seleccionarMascotaJugador()
     let inputTucanazo = document.getElementById('tucanazo')
     let inputPedos = document.getElementById('pedos')
 
-    let mascotaSeleccionada = "Ninguna"
-    let spanMascotaJugador = document.getElementById('mascota-jugador')
+    let selectedPet = "Ninguna"
+    let spanPetPlayer = document.getElementById('mascota-jugador')
 
     const inputs = [ inputHipoperro, inputCapipepo, inputRatigueya, inputLangosucio, inputTucanazo, inputPedos ]
 
-    for (i = 0; i < mascotas.length; i++)
+    for (i = 0; i < pets.length; i++)
     {
         if (inputs[i].checked)
         {
-            mascotaSeleccionada = mascotas[i]
-            spanMascotaJugador.innerHTML = mascotaSeleccionada
-            seleccionarMascotaEnemigo()
+            selectedPet = pets[i]
+            spanPetPlayer.innerHTML = selectedPet
+            selectPetEnemy()
             break
         }
     }
-    if (mascotaSeleccionada == "Ninguna")
+    if (selectedPet == "Ninguna")
     {
         alert("Selecciona una mascota, por favor")
         return 1;
     }
 }
-
-function seleccionarMascotaEnemigo()
+function selectPetEnemy()
 {
-    let mascotaAleatoria = aleatorio(1,6)
-    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
-    spanMascotaEnemigo.innerHTML = mascotas[mascotaAleatoria - 1]
+    let randomPet = randomNum(1,6)
+    let spanPetEnemy = document.getElementById('mascota-enemigo')
+    spanPetEnemy.innerHTML = pets[randomPet - 1]
 
     //now the 3 elemental attack buttons are activated
-    let botonFuego = document.getElementById('boton-fuego')
-    let botonAgua = document.getElementById('boton-agua')
-    let botonTierra = document.getElementById('boton-tierra')
+    let buttonFire = document.getElementById('boton-fuego')
+    let buttonWater = document.getElementById('boton-agua')
+    let buttonEarth = document.getElementById('boton-tierra')
 
-    botonFuego.addEventListener('click', ataqueFuego)
-    botonAgua.addEventListener('click', ataqueAgua)
-    botonTierra.addEventListener('click', ataqueTierra)
+    buttonFire.addEventListener('click', attackFire)
+    buttonWater.addEventListener('click', attackWater)
+    buttonEarth.addEventListener('click', attackEarth)
 }
+function attackFire()
+{
+    attackPlayer = 'FUEGO'
+    combat()
+}
+function attackWater()
+{
+    attackPlayer = 'AGUA'
+    combat()
+}
+function attackEarth()
+{
+    attackPlayer = 'TIERRA'    
+    combat()       
+}
+function combat()
+{
+    attackEnemyRandom()
 
-function ataqueFuego()
-{
-    ataqueJugador = 'FUEGO'
-    ataqueEnemigoAleatorio()
-    alert('Tu mascota atacó con ' + ataqueJugador + ' y la mascota enemiga atacó con ' + ataqueEnemigo + '.')
-}
-function ataqueAgua()
-{
-    ataqueJugador = 'AGUA'
-    ataqueEnemigoAleatorio()
-    alert('Tu mascota atacó con ' + ataqueJugador + ' y la mascota enemiga atacó con ' + ataqueEnemigo + '.')
-}
-function ataqueTierra()
-{
-    ataqueJugador = 'TIERRA'
-    ataqueEnemigoAleatorio()
-    alert('Tu mascota atacó con ' + ataqueJugador + ' y la mascota enemiga atacó con ' + ataqueEnemigo + '.')
-}
-function ataqueEnemigoAleatorio()
-{
-    let ataqueAleatorio = aleatorio(1,3)
-
-    if (ataqueAleatorio == 1)
-    { ataqueEnemigo = 'FUEGO'}
-    else if (ataqueAleatorio == 2)
-    { ataqueEnemigo = 'AGUA'}
+    if (attackPlayer == attackEnemy)
+    {attackResult = 'EMPATE'}
+    else if (attackPlayer == 'FUEGO' && attackEnemy == 'TIERRA' || attackPlayer == 'AGUA' && attackEnemy == 'FUEGO' || attackPlayer == 'TIERRA' && attackEnemy == 'AGUA')
+    {attackResult = 'GANASTE'}
     else
-    { ataqueEnemigo = 'TIERRA'}
-}
+    {attackResult = 'PERDISTE'}
 
-function aleatorio(min, max)
+    printMessage() 
+}
+function attackEnemyRandom()
+{
+    let attackRandom = randomNum(1,3)
+
+    if (attackRandom == 1)
+    { attackEnemy = 'FUEGO'}
+    else if (attackRandom == 2)
+    { attackEnemy = 'AGUA'}
+    else
+    { attackEnemy = 'TIERRA'}
+}
+function printMessage()
+{
+    alert('Tu mascota atacó con ' + attackPlayer + ' y la mascota enemiga atacó con ' + attackEnemy + ' - ' + attackResult + '.')
+}
+function randomNum(min, max)
 {
     return Math.floor(Math.random()*(max-min+1)+min)
 }
 
-window.addEventListener('load', iniciarJuego)
+window.addEventListener('load', startGame)
